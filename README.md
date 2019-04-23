7df7c158e09e1f522c57ef3cc2cd1ed9be130e95
Glitch (special-artichoke) <none>
2019-04-23 15:38:22 +0000

    Updated for: special-artichoke edit 8239

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 **HI** Jonze
-edit 2380
+edit 8239
--- public/client.js
+++ public/client.js
@@ -5,22 +5,22 @@
 // add other scripts at the bottom of index.html
 
 $(function() {
-  console.log('hello world :o');
+  console.log('hello world :o')
   
   $.get('/dreams', function(dreams) {
     dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
+      $('<li></li>').text(dream).appendTo('ul#dreams')
+    })
+  })
 
   $('form').submit(function(event) {
-    event.preventDefault();
-    var dream = $('input').val();
+    event.preventDefault()
+    var dream = $('input').val()
     $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
+      $('<li></li>').text(dream).appendTo('ul#dreams')
+      $('input').val('')
+      $('input').focus()
+    })
+  })
 
-});
+})
